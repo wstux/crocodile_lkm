ed234d425d22663e51e2158e0b8592d9ad874ba3
Chistyakov Alexander <wstux1@gmail.com>
2023-08-05 13:38:04 +0300

    + croc/device: #5 Added utild for char devices;

---
+++ drivers/croc/device/details/cdev_utils.c
@@ -0,0 +1,43 @@
+/*
+ * crocodile_lkm
+ * Copyright (C) 2023  Chistyakov Alexander
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "device/details/cdev_utils.h"
+#include "logging/logger.h"
+
+void cdev_setup(module_dev_t* p_dev, int major, int minor, int idx, 
+                struct file_operations* p_fops)
+{
+    rc_t err;
+    int devno = MKDEV(major, minor + idx);
+
+    cdev_init(&p_dev->cdev, p_fops);
+    p_dev->cdev.owner = THIS_MODULE;
+    err = cdev_add(&p_dev->cdev, devno, 1);
+
+    /* Fail gracefully if need be */
+    if (err) {
+        KLOG_NOTICE(LOG_PREFIX "error %d adding " MODULE_NAME "%d", err, idx);
+    }
+}
+
+rc_t cdev_trim(module_dev_t* p_dev)
+{
+    /* Do nothing now. */
+    return 0;
+}
+
